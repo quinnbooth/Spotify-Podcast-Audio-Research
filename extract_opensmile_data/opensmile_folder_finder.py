f57cfad90240d073d49035d98e3cdcd5822dccca
Lara Karacasu <larakaracasu@gmail.com>
2023-04-05 19:03:56 -0400

    Update opensmile_folder_finder.py

--- extract_opensmile_data/opensmile_folder_finder.py
+++ extract_opensmile_data/opensmile_folder_finder.py
@@ -9,6 +9,7 @@ from selenium.common.exceptions import NoSuchElementException
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
+from selenium.common.exceptions import StaleElementReferenceException
 import csv
 import time
 
@@ -19,14 +20,11 @@ for row in df.show_filename_prefix:
     show = row[0]
     prefix = show[6:8]
     prefixes.add(prefix)
-print(prefixes)
-print(len(prefixes))
 
 driver = webdriver.Chrome(
   executable_path='/usr/lib/chromium-browser/chromedriver')
 driver.get('https://app.box.com/login')
 driver.implicitly_wait(10)
-driver.maximize_window()
 
 username = "xxx"
 uname = driver.find_element("id", "login-email") 
@@ -42,7 +40,7 @@ password_field = WebDriverWait(driver, 10).until(
 )
 
 # Enter your password and click on the "Log In" button
-password = "XXX"
+password = "xxx"
 password_field.send_keys(password)
 login_button = driver.find_element("id", "login-submit-password")
 login_button.click()
@@ -58,3 +56,34 @@ link_element.click()
 # Click on OpenSmile
 link_element = driver.find_element(by=By.XPATH, value='//a[@href="/folder/140172208712"]')
 link_element.click()
+
+# Now, we need to iterate through the prefixes and download the folders from the corresponding links
+folder_elements = driver.find_elements(by=By.XPATH, value="//div[contains(@class, 'ReactVirtualized__Table__row')]")
+
+folder_elements = driver.find_elements(by=By.XPATH, value="//div[contains(@class, 'ReactVirtualized__Table__row') and @data-item-index >= '0' and @data-item-index <= '7']")
+for folder in folder_elements:
+    try:
+        folder_index = folder.get_attribute("data-item-index")
+        folder_id = folder.get_attribute("data-resin-folder_id")
+        folder_name = folder.text
+        if folder_index is not None and int(folder_index) >= 0 and int(folder_index) <= 7:
+            print(folder_index)
+            print(folder_id)
+            print(folder_name)
+    except StaleElementReferenceException:
+        # refresh the page and find the element again
+        driver.refresh()
+        time.sleep(5) # wait 5 seconds before finding elements again
+        folder_elements = driver.find_elements(by=By.XPATH, value="//div[contains(@class, 'ReactVirtualized__Table__row') and @data-item-index >= '0' and @data-item-index <= '7']")
+        continue
+
+
+
+    """
+    if folder_name.startswith(folder_prefix) and folder_id not in downloaded_folders:
+        # download the folder
+        downloaded_folders.add(folder_id)
+        folder.click()
+        # do something to download the files in this folder
+        # then go back to the previous page with driver.back()
+    """
