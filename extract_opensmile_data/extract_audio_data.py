056c56be0ef87da9b1071e672917b6e74e3f6451
Lara Karacasu <larakaracasu@gmail.com>
2023-04-09 16:41:18 -0400

    Added subdirectory zip folder functionalities.

--- extract_opensmile_data/extract_audio_data.py
+++ extract_opensmile_data/extract_audio_data.py
@@ -10,6 +10,8 @@ import numpy as np
 import os
 import pathlib
 import ast
+import zipfile
+import shutil
 
 ## NOTE: place this file in the same directory as metadata CSV, and as openSMILE folder
 ## OR: modify filepaths below
@@ -20,49 +22,54 @@ outfile_name = "refined_metadata_v2.3.csv"
 
 feature_names = df.columns[11:] # change 2 to the column index at which the feature columns begin
 
-# Iterate through H5 subdirectories
-directory = 'sample_h5_dir' # change to the name of main directory
-for subdir in os.listdir(directory):
-    
-    # Check that subdir is a podcast show directory
-    if subdir[:4] == "show":
-        show_URI = subdir
-        df['show_filename_prefix'] = df['show_filename_prefix'].apply(lambda x: ast.literal_eval(x)[0] if subdir in x else x)
-        subdir_path = os.path.join(directory, subdir)
-        show_feature_means = []
-        # Iterate through files in subdir
-        for filename in os.listdir(subdir_path):
-            file = os.path.join(subdir_path, filename)
-            file_extension = pathlib.Path(file).suffix
-            
-            # Check that file is an H5 file
-            if os.path.isfile(file) and file_extension == '.h5':
-                episode_URI = filename[:-3]
+# Iterate through H5 subdirectories                
+# Iterate through H5 subdirectories                
+directory = 'openSMILE' 
+for filename in os.listdir(directory):
+    if filename.endswith(".zip"):
+        zip_file = os.path.join(directory, filename)
+        with zipfile.ZipFile(zip_file, 'r') as zip_ref:
+            subfolder_name = os.path.splitext(filename)[0] # extract the name of the subfolder
+            zip_ref.extractall(directory) # extract the zip contents to the main directory
+            subdir_path = os.path.join(directory, subfolder_name)
+            for subdir in os.listdir(subdir_path):
+                if subdir.startswith("show"):
+                    show_URI = subdir
+                    df['show_filename_prefix'] = df['show_filename_prefix'].apply(lambda x: ast.literal_eval(x)[0] if subdir in x else x)
+                    subdir_path = os.path.join(subdir_path, subdir)
+                    show_feature_means = []
+                    for filename in os.listdir(subdir_path):
+                        file = os.path.join(subdir_path, filename)
+                        file_extension = pathlib.Path(file).suffix
+                        if file_extension == '.h5':
+                            episode_URI = filename[:-3]
                 
-                # Extract OpenSMILE data
-                with h5py.File(file, "r") as f:
-                    for group in f.keys():
-                        members = f[group].keys()
-                        for i in range(len(members)):
-                            member = list(members)[i]
-                            if i == 3: # Holds audio feature values
-                                dataset = f[group][member][:]
-                                
-                                # Each row represents the feature values within a 0.48s interval
-                                # Each column represents a distinct feature
-                                ## Hence, 88 columns
-                                # Should take the mean over columns, not rows
-                                
-                                episode_feature_means = dataset.mean(axis = 0)
-                                show_feature_means.append(episode_feature_means)
-                                ## print(feature_means)
-                                ## print(len(feature_means)) -- Should equal 88 (one mean val per feature)
+                            # Extract OpenSMILE data
+                            with h5py.File(file, "r") as f:
+                                for group in f.keys():
+                                    members = f[group].keys()
+                                    for i in range(len(members)):
+                                        member = list(members)[i]
+                                        if i == 3: # Holds audio feature values
+                                            dataset = f[group][member][:]
+                                            
+                                            # Each row represents the feature values within a 0.48s interval
+                                            # Each column represents a distinct feature
+                                            ## Hence, 88 columns
+                                            # Should take the mean over columns, not rows
+                                            
+                                            episode_feature_means = dataset.mean(axis = 0)
+                                            show_feature_means.append(episode_feature_means)
+                                            ## print(feature_means)
+                                            ## print(len(show_feature_means)) # -- Should equal 88 (one mean val per feature)
+                                            
+                            os.remove(file)
     
-        show_feature_means = np.array(show_feature_means)
-        for i in range(len(feature_names)):
-            feature_name = feature_names[i]
-            val_array = show_feature_means[:, i]
-            df.loc[df.show_filename_prefix == show_URI, feature_name] = np.mean([val_array])
-
-# Export to CSV
-df.to_csv(outfile_name)             
+                    show_feature_means = np.array(show_feature_means)
+                    for i in range(len(feature_names)):
+                        feature_name = feature_names[i]
+                        val_array = show_feature_means[:, i]
+                        df.loc[df.show_filename_prefix == show_URI, feature_name] = np.mean([val_array])
+                        
+                    # Remove the episode directory
+                    os.rmdir(subdir_path)
